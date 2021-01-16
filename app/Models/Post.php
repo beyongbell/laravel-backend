1a5eb7a2d19ecd8c24b6346d4bbb92aa8d22febe
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-16 13:27:04 +0700

    Topic User Post relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
 }
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Topic extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
