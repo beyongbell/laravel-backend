15231ac133c29c380661c387df0f1ddb346a20a2
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-17 23:31:58 +0700

    Add Id User Response

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -15,6 +15,7 @@ class User extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'         => $this->id,
             'name'       => $this->name,
             'email'      => $this->email,
             'created_at' => $this->created_at
