fce61768692b448ad7f2bb3af44d13b6b517d39b
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-17 12:41:34 +0700

    Topic request validation

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Post;
+use App\Http\Requests\TopicCreateRequest;
 use App\Http\Resources\Topic as TopicResource;
 
 class TopicController extends Controller
 {
-    public function store(Request $request)
+    public function store(TopicCreateRequest $request)
     {
         $topic = new Topic;
         $topic->title = $request->title;
